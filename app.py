67eaae0348a9b0f13eaae1a797c2c5a1d3f4306b
ronaldLjin <ronyjin123@gmail.com>
2023-11-11 15:58:05 -0500

    basic detect endpoint working

--- app.py
+++ app.py
@@ -1,7 +1,42 @@
-from flask import Flask
+from flask import Flask, flash, request, redirect
+from detect import detect_items
+from PIL import Image
+from io import BytesIO
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+def is_valid_image(image_data):
+    try:
+        with Image.open(BytesIO(image_data)) as img:
+            img.verify()
+            return True
+    except:
+        return False
+
+
+@app.route("/detect", methods=["POST"])
+def detect():
+    if "image" not in request.files:
+        return "No image provided", 400
+
+    file = request.files["image"]
+
+    if file.filename == "":
+        return "No selected image", 400
+
+    # Read image data from the file
+    image_data = file.read()
+
+    if is_valid_image(image_data):
+        results = detect_items(image_data).detections
+        return results
+    else:
+        return "Invalid file upload", 400
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
--- detect.py
+++ detect.py
@@ -2,21 +2,29 @@ import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
-MODEL_PATH = 'model/efficientdet_lite0.tflite'
+from io import BytesIO
+from PIL import Image
+import numpy as np
 
-# Load the input image from an image file.
-MP_IMAGE = mp.Image.create_from_file('test-data/518-Px9udpL._AC_UF1000,1000_QL80_.jpg')
+MODEL_PATH = "model/efficientdet_lite0.tflite"
 
-BaseOptions = mp.tasks.BaseOptions
-ObjectDetector = mp.tasks.vision.ObjectDetector
-ObjectDetectorOptions = mp.tasks.vision.ObjectDetectorOptions
-VisionRunningMode = mp.tasks.vision.RunningMode
 
-options = ObjectDetectorOptions(
-    base_options=BaseOptions(model_asset_path=MODEL_PATH),
-    max_results=5,
-    running_mode=VisionRunningMode.IMAGE)
+def detect_items(image_data):
+    # Load the input image from an image file.
+    image = np.array(Image.open(BytesIO(image_data)))
 
-with ObjectDetector.create_from_options(options) as detector:
-    detection_result = detector.detect(MP_IMAGE)
-    print(detection_result)
+    image = mp.Image(image_format=mp.ImageFormat.SRGB, data=image)
+    BaseOptions = mp.tasks.BaseOptions
+    ObjectDetector = mp.tasks.vision.ObjectDetector
+    ObjectDetectorOptions = mp.tasks.vision.ObjectDetectorOptions
+    VisionRunningMode = mp.tasks.vision.RunningMode
+
+    options = ObjectDetectorOptions(
+        base_options=BaseOptions(model_asset_path=MODEL_PATH),
+        max_results=5,
+        running_mode=VisionRunningMode.IMAGE,
+    )
+
+    with ObjectDetector.create_from_options(options) as detector:
+        detection_result = detector.detect(image)
+        return detection_result
