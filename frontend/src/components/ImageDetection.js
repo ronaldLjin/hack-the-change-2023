f9d42126e47c8f5756bc7e690ec1b2fbb2585907
Jason Wang <jasonbln07@gmail.com>
2023-11-11 23:42:22 -0500

    fix example card category section

--- frontend/src/components/ImageDetection.js
+++ frontend/src/components/ImageDetection.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import {
   Image,
   Card,
@@ -12,9 +12,18 @@ import {
   Tooltip,
   IconButton,
   Box,
-} from '@chakra-ui/react';
-import { CloseIcon } from '@chakra-ui/icons';
-import axios from 'axios';
+} from "@chakra-ui/react";
+import { CloseIcon, ChevronRightIcon, ChevronDownIcon } from "@chakra-ui/icons";
+import axios from "axios";
+
+const exampleCard = {
+  imageSrc: "/trashcans.jpg",
+  imageAlt: "Multiple Recycling Bins",
+  items: [
+    { categories: [{ category_name: "bottle", text: "recycling" }] },
+    { categories: [{ category_name: "egg shell", text: "compost" }] },
+  ],
+};
 
 export default function ImageDetection({
   image,
@@ -24,7 +33,7 @@ export default function ImageDetection({
   showMap,
   setShowMap,
 }) {
-  const [items, setItems] = useState();
+  const [items, setItems] = useState(undefined);
   const [categories, setCategories] = useState({});
   const [isLoading, setIsLoading] = useState(false);
 
@@ -38,9 +47,9 @@ export default function ImageDetection({
   function detect() {
     setIsLoading(true);
     const formData = new FormData();
-    formData.append('image', file);
-    fetch('http://localhost:5000/detect', {
-      method: 'POST',
+    formData.append("image", file);
+    fetch("http://localhost:5000/detect", {
+      method: "POST",
       body: formData,
     })
       .then((res) => res.json())
@@ -64,101 +73,108 @@ export default function ImageDetection({
   const closeDetection = () => {
     setImage(undefined);
     setFile(undefined);
+    setItems(undefined);
+    setCategories({});
   };
 
   return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          margin: '10px 0',
-        }}
-      >
-        <Card maxW="sm">
-          <CardBody>
-            {!image && <Heading size="sm">Example Detection Card!</Heading>}
-            {image && (
-              <Tooltip label="Close Detection" placement="left">
-                <IconButton
-                  variant="ghost"
-                  aria-label="Close Image"
-                  size="sm"
-                  margin="0"
-                  icon={<CloseIcon />}
-                  onClick={closeDetection}
-                />
-              </Tooltip>
-            )}
-            <Stack mt="2" spacing="2">
-              <Image
-                src={image ?? '/trashcans.jpg'}
-                alt="selected file"
-                borderRadius="lg"
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        margin: "10px 0",
+      }}
+    >
+      <Card maxW="sm">
+        <CardBody>
+          {!image && <Heading size="sm">Example Detection Card!</Heading>}
+          {image && (
+            <Tooltip label="Close Detection" placement="left">
+              <IconButton
+                variant="ghost"
+                aria-label="Close Image"
+                size="sm"
+                margin="0"
+                icon={<CloseIcon />}
+                onClick={closeDetection}
               />
-              <Button onClick={detect} isDisabled={!image}>
-                Detect
-              </Button>
-            </Stack>
-            <Stack mt="6" spacing="3">
-              <Heading size="md">Category of Disposal</Heading>
-              {items?.map((item) => {
-                return (
-                  <Test
-                    key={Math.random(21230192391203)}
-                    item={item}
-                    categories={categories}
-                    isLoading={isLoading}
-                  />
-                );
-              })}
-            </Stack>
-          </CardBody>
-          <Divider />
-          <CardFooter>
-            <div
-              style={{
-                display: 'flex',
-                alignItems: 'center',
-                columnGap: '4px',
-              }}
+            </Tooltip>
+          )}
+          <Stack mt="2" spacing="2">
+            <Image
+              src={image ?? exampleCard.imageSrc}
+              alt={exampleCard.imageAlt}
+              borderRadius="lg"
+            />
+            <Button onClick={detect} isDisabled={!image}>
+              Detect
+            </Button>
+          </Stack>
+          <Stack mt="6" spacing="3">
+            <Heading size="md">Category of Disposal</Heading>
+            {console.log(items)}
+            {(items ?? (image ? [] : exampleCard.items)).map((item) => {
+              return (
+                <ItemList
+                  key={Math.random(21230192391203)}
+                  item={item}
+                  categories={categories}
+                  isLoading={isLoading}
+                  useExample={!image}
+                />
+              );
+            })}
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              columnGap: "4px",
+            }}
+          >
+            <Text color="red.600" fontSize="lg">
+              Safety reminder if its stuff like a battery?
+            </Text>
+            <Button
+              width="150px"
+              variant="solid"
+              colorScheme="blue"
+              onClick={() => setShowMap((prev) => !prev)}
             >
-              <Text color="red.600" fontSize="lg">
-                Safety reminder if its stuff like a battery?
-              </Text>
-              <Button
-                width="150px"
-                variant="solid"
-                colorScheme="blue"
-                onClick={() => setShowMap((prev) => !prev)}
-              >
-                {showMap ? 'Close Map' : 'View Map'}
-              </Button>
-            </div>
-          </CardFooter>
-        </Card>
-      </div>
-    </>
+              {showMap ? "Close Map" : "View Map"}
+            </Button>
+          </div>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
-function Test({ item, categories, isLoading }) {
+function ItemList({ item, categories, isLoading, useExample }) {
+  const [isOpen, setIsOpen] = useState(false);
   const category = item.categories[0].category_name;
-
-  const [open, setIsOpen] = useState(false);
 
   return (
     <>
       <Button
         isDisabled={isLoading}
+        leftIcon={isOpen ? <ChevronDownIcon /> : <ChevronRightIcon />}
         onClick={() => {
           if (!isLoading) {
-            setIsOpen(true);
+            setIsOpen((prev) => !prev);
           }
         }}
       >
         {category}
       </Button>
-      {open && <Box>Category: {categories[category]}</Box>}
+      {isOpen && (
+        <Box>
+          Category:{" "}
+          {useExample ? item.categories[0].text : categories[category]}
+        </Box>
+      )}
     </>
   );
 }
